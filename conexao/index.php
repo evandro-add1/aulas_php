4fb25747c6c96c0e0abef61272fa788239fc10be
Evandro <evandroadd1@gmail.com>
2026-05-11 22:21:33 -0300

    ja ja muda tudo

--- conexao/index.php
+++ conexao/index.php
@@ -1,21 +1,19 @@
 <?php
 
-$dsn = "mysql:host=localhost;dbname=exemplo; port=3307";
+$dsn = "mysql:host=localhost;dbname=dbe; port=3307";
 $username = "root";
 $password = "";
 $pdo = new PDO($dsn, $username, $password);
 
+$resultado = $pdo->query("select * from usuario");
 
 if ($resultado->rowCount()>0){
     while($usuario = $resultado->fetch()){
         echo "<pre>";
         var_dump($usuario);
-        echo "ID: ". $usuario["id"]. "<br";
+        echo "ID: ". $usuario["id"]. "<br>";
         echo "Usuario: ". $usuario["usuario"]. "<br>";
         echo "Senha: **** ";
-
-        
-
     }
 }
 else {
